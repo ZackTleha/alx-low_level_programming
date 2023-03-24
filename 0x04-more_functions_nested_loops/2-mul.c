6e7756ef4cd027dbfa46a84039bb0de42e77d801
ZackTleha <itsmezacc@gmail.com>
2023-03-24 14:43:24 -0700

    c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul  - check the code
+ * @a: variable
+ * @b: variable
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - check the code
+ * @n : variable
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+
+	_putchar('\n');
+}
